27fe4586343c6b9372b74d2a6c48dd16e343223f
Cohere.img <cohere.img@gmail.com>
2025-04-14 00:31:29 +0900

    Fix:microCMS

--- libs/client.ts
+++ libs/client.ts
@@ -62,18 +62,18 @@ export type Category = {
     name: string;
 } & MicroCMSListContent;
 
-if (!process.env.SERVICE_DOMAIN) {
-    throw new Error("SERVICE_DOMAIN is required");
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+    throw new Error("MICROCMS_SERVICE_DOMAIN is required");
 }
 
-if (!process.env.API_KEY) {
-    throw new Error("API_KEY is required");
+if (!process.env.MICROCMS_API_KEY) {
+    throw new Error("MICROCMS_API_KEY is required");
 }
 
 // microCMS クライアントの作成
 export const client = createClient({
-    serviceDomain: process.env.SERVICE_DOMAIN || "",
-    apiKey: process.env.API_KEY || "",
+    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || "",
+    apiKey: process.env.MICROCMS_API_KEY || "",
 });
 
 /**
