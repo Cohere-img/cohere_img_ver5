53e9b4e610ec61c0f0fb1aa3b018be47e5dcc0ff
Cohere.img <cohere.img@gmail.com>
2025-04-14 00:47:39 +0900

    Fix: microCMSimg

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
         MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
         MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
     },
+    images: {
+        domains: ["images.microcms-assets.io"],
+    },
     eslint: {
         ignoreDuringBuilds: true,
     },
