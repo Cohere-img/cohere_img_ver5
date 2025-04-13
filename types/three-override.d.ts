5f1ab0c986ce573064e4d99fa23f0f6ae14b9a2b
Cohere.img <cohere.img@gmail.com>
2025-04-13 23:20:48 +0900

    Fix: remove missing TextLoop import to pass Vercel build

---
+++ types/three-override.d.ts
@@ -0,0 +1,8 @@
+// types/three-override.d.ts
+
+declare module "three/examples/jsm/postprocessing/EffectComposer";
+declare module "three/examples/jsm/postprocessing/RenderPass";
+declare module "three/examples/jsm/postprocessing/UnrealBloomPass";
+declare module "three/examples/jsm/postprocessing/BokehPass";
+declare module "three/examples/jsm/shaders/VignetteShader";
+declare module "three/examples/jsm/postprocessing/ShaderPass";
